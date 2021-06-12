a7ebcb4b6934363e1732dac10ae7acb1132da86e
Samuel Kavin <samuelkavin.loganathan@cimb.com>
2021-06-13 00:22:59 +0800

    change the email

--- src/templates/mail.php
+++ src/templates/mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "samuelkavin@yahoo.com";
+        $mail_to = "enquiry@acgil.tech";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
